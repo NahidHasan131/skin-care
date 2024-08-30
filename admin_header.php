83809c31f738b65b2a4ea7489f6e550a5bd7ca1f
Md. Nahid Hasan <nahid7532140@gmail.com>
2024-08-31 01:48:42 +0600

    admin panel

--- admin_header.php
+++ admin_header.php
@@ -15,26 +15,27 @@ if(isset($message)){
 
    <div class="flex">
 
-      <a href="admin_page.php" class="logo">Admin<span>Panel</span></a>
+      <a href="admin_page.php" class="logo"><i class="fa-solid fa-shield-virus"></i>Admin<span>Panel</span></a>
 
       <nav class="navbar">
-         <a href="admin_page.php">home</a>
-         <a href="admin_products.php">products</a>
-         <a href="admin_orders.php">orders</a>
-         <a href="admin_users.php">users</a>
-         <a href="admin_contacts.php">messages</a>
+         <a href="admin_page.php"><i class="fa-solid fa-house-user"></i> home</a>
+         <a href="admin_products.php"><i class="fa-solid fa-cart-shopping"></i> products</a>
+         <a href="admin_orders.php"><i class="fa-solid fa-truck-fast"></i> orders</a>
+         <a href="admin_users.php"><i class="fa-solid fa-user-plus"></i> users</a>
+         <a href="admin_contacts.php"><i class="fa-regular fa-comment"></i> messages</a>
       </nav>
 
       <div class="icons">
          <div id="menu-btn" class="fas fa-bars"></div>
-         <div id="user-btn" class="fas fa-user"></div>
+         <div id="user-btn" class="fa-solid fa-user-gear"></div>
+
       </div>
 
       <div class="account-box">
-         <p>username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
-         <p>email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
+         <p>Username : <span><?php echo $_SESSION['admin_name']; ?></span></p>
+         <p>Email : <span><?php echo $_SESSION['admin_email']; ?></span></p>
          <a href="logout.php" class="delete-btn">logout</a>
-         <div>new <a href="login.php">login</a> | <a href="register.php">register</a></div>
+         <div>New <a href="login.php">login</a> | <a href="register.php">register</a></div>
       </div>
 
    </div>
