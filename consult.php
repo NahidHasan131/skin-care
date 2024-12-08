ad9a8580b4080011961af92abf95178d1c6fe738
Md. Nahid Hasan <nahid7532140@gmail.com>
2024-12-09 00:53:15 +0600

    changes

--- consult.php
+++ consult.php
@@ -39,21 +39,21 @@ if(!isset($user_id)){
    <div class="box-container">   
       <div class="box">
          <img src="./images/dr-female.jpg" alt="">
-         <div class="name">Dr.HS Yesmin</div>
+         <div class="name">Dr. SSS</div>
          <div class="designantion">MBBS, BCS (Health), FCPS (Skin & VD)</div>
          <a href="tel:+88017000000" class="option-btn">Call</a>
          <a href="mailto:someone@example.com" class="delete-btn">Mail Us</a>
       </div>
       <div class="box">
          <img src="./images/dr-female.jpg" alt="">
-         <div class="name">Dr.JN Jeba</div>
+         <div class="name">Dr. JJJ</div>
          <div class="designantion">Dermatologist</div>
          <a href="tel:+88017000000" class="option-btn">Call</a>
          <a href="mailto:someone@example.com" class="delete-btn">Mail Us</a>
       </div>
       <div class="box">
          <img src="./images/dr-female.jpg" alt="">
-         <div class="name">Dr.Shrabony</div>
+         <div class="name">Dr. ZZZ</div>
          <div class="designantion">Skin Specialist</div>
          <a href="tel:+88017000000" class="option-btn">Call</a>
          <a href="mailto:someone@example.com" class="delete-btn">Mail Us</a>
@@ -64,19 +64,19 @@ if(!isset($user_id)){
 </section>
 
 <!-- Video List -->
-<h1 class="title show-consult-title"><i class="fa-solid fa-prescription"></i> Consult from Related Video <i class="fa-solid fa-stethoscope"></i></h1>
+<h1 class="title show-consult-title"><i class="fa-solid fa-prescription"></i> Buy Products from Related Video <i class="fa-solid fa-stethoscope"></i></h1>
 <section class="Show-video">
     <div class="box-container">
         <div class="box">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/VeWTvUuiGBo?si=Y44XBYvktoqBhq3Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/BkRb-TsG9mI?si=RPRpbMaIR3qkUwr-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div class="box">
-        <iframe width="560" height="315"  src="https://www.youtube.com/embed/Texh6bwbHgM?si=KUkb45ivKFF8T-S5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315"  src="https://www.youtube.com/embed/wFTPf7K71Ww?si=5gms2CYOKfj5sIEE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     </div>
     <div class="box-container">
         <div class="box">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/Uf45R6Ld3zI?si=cutpkJTpWoN49F5f" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/6flEzueaPck?si=O5BkOJYq8ZZHEwu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div class="box">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/M3Q4zRhXluw?si=euHq5XoAU-RzHY47" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
@@ -84,7 +84,7 @@ if(!isset($user_id)){
     </div>
     <div class="box-container">
         <div class="box">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/5QvoCf5MKao?si=pTPcGrPOZxdnTkk1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/pKsfni_JhJY?si=GiVsoBDA9Ld6pmu2" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
         <div class="box">
         <iframe width="560" height="315" src="https://www.youtube.com/embed/DkIoSnLI4yk?si=2gXCxm7yoR3z3aKN" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
